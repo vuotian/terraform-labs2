4721267f8e366ed94c34438362a812dd9b50522e
AnttiV <antti.vuoti@gmail.com>
2020-04-09 12:51:17 +0300

    security rules

--- NSGs.tf
+++ NSGs.tf
@@ -77,6 +77,21 @@ resource "azurerm_network_security_rule" "AllowSQLServer" {
     source_address_prefix       = "*"
 }
 
+resource "azurerm_network_security_rule" "AllowRDP" {
+    name = "AllowRDP"
+    resource_group_name         = azurerm_resource_group.nsgs.name
+    network_security_group_name = azurerm_network_security_group.resource_group_default.name
+
+    priority                    = 1040
+    access                      = "Allow"
+    direction                   = "Inbound"
+    protocol                    = "Tcp"
+    destination_port_range      = 3389
+    destination_address_prefix  = "*"
+    source_port_range           = "*"
+    source_address_prefix       = "*"
+
+
 resource "azurerm_network_security_group" "nic_ubuntu" {
    name = "NIC_Ubuntu"
    resource_group_name  = azurerm_resource_group.nsgs.name
@@ -94,4 +109,24 @@ resource "azurerm_network_security_group" "nic_ubuntu" {
         source_address_prefix      = "*"
         destination_address_prefix = "*"
   }
+}
+
+resource "azurerm_network_security_group" "nic_windows" {
+    name = "NIC_Windows"
+    resource_group_name = azurerm_resource_group.nsgs.name
+    location = azurerm_resource_group.nsgs.location
+    tags = azurerm_resource_group.nsgs.tags
+
+    security_rule {
+        name = "AllowRDP"
+        priority = 100
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_port_range = "*"
+        destination_port_range = 3389
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+    }
+
 }
